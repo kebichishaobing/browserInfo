34a2a8611ed5f1c44caa1351248cede9375294e7
Vincent Chang <vincent.chang@morningstar.com>
2017-05-03 19:50:48 +0800

    add engine and broser check

--- browserInfo.js
+++ browserInfo.js
@@ -1,4 +1,4 @@
-var client = function() {
+var browserInfo = function() {
 
     var engine = {
         //type
@@ -43,6 +43,64 @@ var client = function() {
         wii: false,
         ps: false
     };
+
+    var ua = navigator.userAgent;
+    if (window.oprea) {
+        engine.version = browser.version = window.oprea.version();
+        engine.oprea = browser.oprea = parseFloat(engine.version);
+    } else if (/AppleWebkit\/(\S+)/.test(ua)) {
+        engine.version = RegExp['$1'];
+        engine.webkit = parseFloat(engine.version);
+
+        // confirm is Chrome or safari
+        if (/Chrome\/(\S+)/.test(ua)) {
+            engine.version = RegExp['$1'];
+            engine.webkit = parseFloat(engine.version);
+        } else if (/Version\/(\S+)/.test(ua)) {
+            engine.version = RegExp['$1'];
+            engine.safari = parseFloat(engine.version);
+        } else {
+            // probably determine the version number
+            var safariVersion = 1;
+            if (engine.webkit < 100) {
+                safariVersion = 1;
+            } else if (engine.webkit < 312) {
+                safariVersion = 1.2;
+            } else if (engine.webkit < 412) {
+                safariVersion = 1.3;
+            } else {
+                safariVersion = 2;
+            }
+
+            browser.safari = browser.version = safariVersion;
+        }
+
+    } else if (/KHTML\/(\S+)/.test(ua) || /Konqueror\/([^;])/.test(ua)) {
+        engine.version = browser.version = RegExp['$1'];
+        engine.khtml = browser.kong = parseFloat(engine.version);
+    } else if (/rv:([^\)]+\) Gecko \/\d{8}/.test(ua)) {
+        engine.version = RegExp['$1'];
+        engine.gecko = parseFloat(engine.version);
+
+        // confirm is Firefox or not
+        if (/Firefox\/(\S+)/.test(ua)) {
+            engine.version = RegExp['$1'];
+            engine.firefox = parseFloat(engine.version);
+        }
+    } else if (/MSIE ([^;]+)/.test(ua)) {
+        engine.version = browser.version = RegExp['$1'];
+        engine.ie = browser.ie = parseFloat(engine.version);
+    }
+
+    // check browser
+    browser.ie = engine.ie;
+    browser.oprea = engine.oprea;
+
+    // check platform
+    var p = navigator.platform;
+    system.win = p.indexOf('Win') == 0;
+    system.mac = p.indexOf('Mac') == 0;
+    system.x11 = (p == 'X11') || (p.indexOf('Linux') == 0);
 
     return {
         engine: engine,
